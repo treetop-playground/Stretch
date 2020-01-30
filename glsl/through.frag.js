7ac4f056ce24a467b312b33df53345720709ce4e
wgalyen <wgalyen@bellhelmets.com>
2020-01-30 10:23:57 -0800

    fbo: base setup

---
+++ glsl/through.frag.js
@@ -0,0 +1,9 @@
+export default /* glsl */`
+precision highp float;
+uniform vec2 tSize;
+uniform sampler2D texture;
+void main() {
+	vec2 uv = gl_FragCoord.xy / tSize.xy;
+	gl_FragColor = texture2D( texture, uv );
+}
+`;
--- js/fbo.js
+++ js/fbo.js
@@ -1,14 +1,95 @@
-let RESOLUTION,
-    renderer, scene, camera, mesh,
-    originalRT, previousRT, positionRT,
-    constraintRTs;
+// shader-import-block
+import quad_vert from '../glsl/quad.vert.js';
+import through_frag from '../glsl/through.frag.js';
+// import position_frag from '../glsl/position.frag.js';
+// import velocity_frag from '../glsl/velocity.frag.js';
 
-function init (renderer, vertices, constraints) {
+let RESOLUTION,
+    renderer, mesh;
+
+// setup
+const tSize = new THREE.Vector2(),
+    scene = new THREE.Scene(),
+    camera = new THREE.Camera(),
+
+    // materials
+    copyShader = new THREE.RawShaderMaterial( {
+        uniforms: {
+            tSize: { type: 'v2', value: tSize },
+            texture: { type: 't' }
+        },
+        vertexShader: quad_vert,
+        fragmentShader: through_frag,
+        fog: false,
+        lights: false,
+        depthWrite: false,
+        depthTest: false
+    } ),
+
+    integrateShader = new THREE.RawShaderMaterial( {
+        uniforms: {
+            tSize: { type: 'v2', value: tSize },
+            texture: { type: 't' }
+        },
+        vertexShader: quad_vert,
+        fragmentShader: through_frag,
+        fog: false,
+        lights: false,
+        depthWrite: false,
+        depthTest: false
+    } ),
+
+   mouseShader = new THREE.RawShaderMaterial({
+       uniforms: {
+           tSize: { type: 'v2', value: tSize },
+           texture: { type: 't' }
+       },
+       vertexShader: quad_vert,
+       fragmentShader: through_frag,
+       fog: false,
+       lights: false,
+       depthWrite: false,
+       depthTest: false
+   }),
+
+   constraintShader = new THREE.RawShaderMaterial({
+       uniforms: {
+           tSize: { type: 'v2', value: tSize },
+           texture: { type: 't' }
+       },
+       vertexShader: quad_vert,
+       fragmentShader: through_frag,
+       fog: false,
+       lights: false,
+       depthWrite: false,
+       depthTest: false
+   }),
+
+    // render targets
+    originalRT = new THREE.WebGLRenderTarget(RESOLUTION, RESOLUTION, {
+        minFilter: THREE.NearestFilter,
+        magFilter: THREE.NearestFilter,
+        format: THREE.RGBAFormat,
+        depthTest: false,
+        depthWrite: false,
+        depthBuffer: false,
+        stencilBuffer: false
+    }),
+
+    constraintRTs = new Array(2),
+    previousRT = originalRT.clone(),
+    positionRT = originalRT.clone();
+
+    constraintRTs[0] = originalRT.clone();
+    constraintRTs[1] = originalRT.clone();
+
+function init(renderer, vertices, constraints) {
 
     // setup
     renderer = renderer;
-    scene = new THREE.Scene();
-    camera = new THREE.Camera();
+
+    RESOLUTION = Math.ceil(Math.sqrt(vertices.count));
+    tSize.set(RESOLUTION, RESOLUTION);
 
     // geometry
     const geometry = new THREE.BufferGeometry();
@@ -20,34 +101,12 @@ function init (renderer, vertices, constraints) {
 
     geometry.addAttribute('position', new THREE.BufferAttribute(positions, 2));
 
-    // materials
-    // copyShader
-    // integrateShader
-    // mouseShader
-    // constraintShader
-
     // mesh
-    // mesh = new THREE.Mesh(geometry, copyShader);
-    // mesh.frustumCulled = false;
-    // scene.add(mesh);
+    mesh = new THREE.Mesh(geometry, copyShader);
+    mesh.frustumCulled = false;
+    scene.add(mesh);
 
-    // render targets
-    originalRT = new THREE.WebGLRenderTarget(RESOLUTION, RESOLUTION, {
-        minFilter: THREE.NearestFilter,
-        magFilter: THREE.NearestFilter,
-        format: THREE.RGBAFormat,
-        depthTest: false,
-        depthWrite: false,
-        depthBuffer: false,
-        stencilBuffer: false
-    });
-
-    previousRT = originalRT.clone();
-    positionRT = originalRT.clone();
-
-    constraintRTs = new Array(2);
-    constraintRTs[0] = originalRT.clone();
-    constraintRTs[1] = originalRT.clone();
+    // prepare
 }
 
 export { init };
