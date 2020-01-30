185cce9a605a794b31722d2f07ec778520e27791
wgalyen <wgalyen@bellhelmets.com>
2020-01-30 09:35:21 -0800

    particle rework & constraints optimization

--- js/particle.js
+++ js/particle.js
@@ -2,25 +2,32 @@ function Particle(x, y, z, mass) {
 
     this.mass = mass;
     this.position = new THREE.Vector3(x, y, z);
-    this.previous = this.position.clone();
-    this.original = this.position.clone();
+    this.previous = new THREE.Vector3(x, y, z);
+    this.original = new THREE.Vector3(x, y, z);
 
     this.adj = [];
     this.distance = 0;
-    this.tmp = new THREE.Vector3();
     this.a = new THREE.Vector3();
 }
 
-Particle.prototype.addForce = function(force) {
-    this.a.copy(this.tmp.copy(force).multiplyScalar(1 / this.mass));
-};
+Object.assign(Particle.prototype, {
 
-Particle.prototype.integrate = function(timesq) {
+    constructor: Particle,
 
-    this.tmp.subVectors(this.position, this.previous);
-    this.tmp.multiplyScalar(DRAG).add(this.position);
-    this.tmp.add(this.a.multiplyScalar(timesq));
+    addForce: function(force) {
+        this.a.copy(force).multiplyScalar(1 / this.mass);
+    },
 
-    this.previous.copy(this.position);
-    this.position.copy(this.tmp);
-}
+    integrate: function(timesq) {
+        const v0 = new THREE.Vector3();
+
+        return function integrate(timesq) {
+            v0.subVectors(this.position, this.previous);
+            v0.multiplyScalar(DRAG).add(this.position);
+            v0.add(this.a.multiplyScalar(timesq));
+
+            this.previous.copy(this.position);
+            this.position.copy(v0);
+        }
+    }()
+});
