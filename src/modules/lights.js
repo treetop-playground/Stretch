ab00447ff41bd366abd40c185d0625eba82951ee
wgalyen <wgalyen@bellhelmets.com>
2020-02-04 14:23:56 -0800

    sim: update constants

--- src/modules/lights.js
+++ src/modules/lights.js
@@ -7,10 +7,12 @@ const
 function init (scene) {
 
     // lights
-    const light = new THREE.AmbientLight(0x455045, 0);
+    const ambientLight = new THREE.AmbientLight(0xeeffe6, 0);
+    ambientLight.baseIntensity = 0.9;
 
     const spotLight = new THREE.SpotLight(0xfd8b8b, 0, 4000, Math.PI / 6, 0.2, 0.11);
     spotLight.position.set(0.9, 0.1, -0.5).multiplyScalar(400);
+    spotLight.baseIntensity = 2.6;
     spotLight.castShadow = true;
     spotLight.shadow.radius = 20;
     spotLight.shadow.camera.far = 4000;
@@ -19,13 +21,15 @@ function init (scene) {
 
     const spotLight2 = new THREE.SpotLight(0x6b7af4, 0, 4000, Math.PI / 6, 0.2, 0.11);
     spotLight2.position.set(-0.91, 0.1, -0.5).multiplyScalar(400);
+    spotLight2.baseIntensity = 2.6;
     spotLight2.castShadow = true;
     spotLight2.shadow.radius = 20;
     spotLight2.shadow.camera.far = 4000;
     spotLight2.shadow.mapSize.height = 4096;
     spotLight2.shadow.mapSize.width = 4096;
 
-    const spotLight3 = new THREE.SpotLight(0x858585, 0, 4000, Math.PI / 5.5, 1.4, 0.02);
+    const spotLight3 = new THREE.SpotLight(0xffffff, 0, 4000, Math.PI / 5.5, 1.4, 0.08);
+    spotLight3.baseIntensity = 1.0;
     spotLight3.position.set(0, 0, -1).multiplyScalar(400);
     spotLight3.castShadow = true;
     spotLight3.shadow.radius = 5;
@@ -33,8 +37,12 @@ function init (scene) {
     spotLight3.shadow.mapSize.height = 4096;
     spotLight3.shadow.mapSize.width = 4096;
 
-    scene.add(light, spotLight, spotLight2, spotLight3);
-    objects = [light, spotLight, spotLight2, spotLight3];
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0);
+    directionalLight.baseIntensity = 0.6;
+    directionalLight.position.set(0, 1, -0.2);
+
+    scene.add(ambientLight, spotLight, spotLight2, spotLight3, directionalLight);
+    objects = [ambientLight, spotLight, spotLight2, spotLight3, directionalLight];
 
 }
 
@@ -51,7 +59,7 @@ function update () {
 
         for (let i = 0; i < objects.length; i++) {
 
-            objects[i].intensity = easing((time - 1) / 3, 2.6);
+            objects[i].intensity = objects[i].baseIntensity * easing((time - 1) / 3, 1.0);
 
         }
 
